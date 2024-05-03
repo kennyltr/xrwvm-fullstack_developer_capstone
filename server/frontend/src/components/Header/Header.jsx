d47321a1e0a1479b22ac6dad71a476398b014508
Kenny Lee <kennyltr@hotmail.com>
2024-05-03 11:59:41 +0800

    week 2 less register

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -38,22 +38,22 @@ if ( curr_user !== null &&  curr_user !== "") {
 }
     return (
         <div>
-          <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
+          <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"rgb(16, 103, 117)",height:"1in"}}>
             <div class="container-fluid">
-              <h2 style={{paddingRight: "5%"}}>Dealerships</h2>
+              <h2 style={{paddingRight: "5%",color:"white"}}>Dealerships</h2>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href="/">Home</a>
+                    <a class="nav-link" style={{fontSize: "larger", color: "white",  fontWeight: "bolder"}} aria-current="page" href="/">Home</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/about">About Us</a>
+                    <a class="nav-link" style={{fontSize: "larger", color: "white"}} href="/about">About Us</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/contact">Contact Us</a>
+                    <a class="nav-link" style={{fontSize: "larger", color: "white"}} href="/contact">Contact Us</a>
                   </li>
                 </ul>
                 <span class="navbar-text">
