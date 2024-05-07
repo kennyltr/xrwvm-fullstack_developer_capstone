10a5274f154929b7984d647c58234f28113cab85
Kenny Lee <kennyltr@hotmail.com>
2024-05-07 11:31:28 +0800

    lint python 6

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -92,8 +92,9 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME':
-     'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    {   
+    'NAME':
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
     },
     {
         'NAME':
